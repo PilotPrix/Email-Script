7b2fa08681213e19de05b968d7f19ccd55dface0
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-05-27 22:26:55 -0400

    Cut email message with extra newlines

--- spreadsheet.py
+++ spreadsheet.py
@@ -7,7 +7,7 @@ sender_email = "wangs2997@wrdsb.ca"
 sender_password = ""
 with open("password 1.txt", "r") as f:
     sender_password = f.read()
-yag = yagmail.SMTP(user=sender_email, password=sender_password)
+yag = yagmail.SMTP(user=sender_email, password=sender_password, port=465)
 
 # gspread initialization for Google Sheets API
 scope = ['https://www.googlapis.com/feeds',
@@ -43,9 +43,16 @@ def send_guest_speaker_emails(x):
         info = guest_speakers.row_values(uncontacted)
         name = info[0]
         template = [
-            f"Hi {name}!",
-            "My name is Peter, I am a high school student and the outreach lead for Highlander Engineering this year. Currently, we are planning on hosting the Highlander Engineering Challenge, a hackathon-style event, in which over 200+ students across Waterloo will be building projects in teams and competing with other high schools. The event will be hosted from May 10 to June 30th.",
-            "We were wondering if you would like to host a workshop or AMA with our students. You could talk about work you've done or a topic with respect to your profession. We believe the students will appreciate having you in our club. Click here for more information about times, dates, and other ideas.",
+            f"Hi {name}!",  
+            "",
+            "My name is Peter, I am a high school student and the outreach lead for Highlander Engineering this year.",
+            "Currently, we are planning on hosting the Highlander Engineering Challenge, a hackathon-style event",
+            "in which over 200+ students across Waterloo will be building projects in teams and competing with other high schools.",
+            "The event will be hosted from May 10 to June 30th.",
+            "",
+            "We were wondering if you would like to host a workshop or AMA with our students. You could talk about work you've done",
+            "or a topic with respect to your profession. We believe the students will appreciate having you in our club.",
+            "Click here for more information about times, dates, and other ideas.",
             "I have also attached our speaker information sheet where you can learn more about what it is like to be a guest speaker at Highlander Engineering.",
             "",
             "Looking forward to hearing from you!",
@@ -57,7 +64,7 @@ def send_guest_speaker_emails(x):
         yag.send(to="seanwaterloo2997@gmail.com", subject="Join Us and Help Us Shape the Next Generation of Engineers", contents=template)
         
         i += 1
-        if i == 5:
+        if i == x:
             break
 
 send_guest_speaker_emails(1)
